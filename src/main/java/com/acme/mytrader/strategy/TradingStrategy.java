be481906f2d132f5ac87c788076aad4aaa814cfe
Praveen Kumar <Pkumar870@github.com>
2020-06-24 21:10:38 -0400

    Added scheduler.

--- src/main/java/com/acme/mytrader/strategy/TradingStrategy.java
+++ src/main/java/com/acme/mytrader/strategy/TradingStrategy.java
@@ -1,5 +1,11 @@
 package com.acme.mytrader.strategy;
 
+import java.util.Timer;
+
+import com.acme.mytrader.execution.ExecutionService;
+import com.acme.mytrader.price.PriceListenerImpl;
+import com.acme.mytrader.price.PriceSource;
+
 /**
  * <pre>
  * User Story: As a trader I want to be able to monitor stock prices such
@@ -7,4 +13,15 @@ package com.acme.mytrader.strategy;
  * </pre>
  */
 public class TradingStrategy {
+
+	public void start(String breachSecurity, int breachVolume, double breachPrice, PriceSource priceSource,
+			ExecutionService executionService) {
+		Timer timer;
+		PriceListenerImpl priceListenerImpl;
+
+		priceListenerImpl = new PriceListenerImpl(breachSecurity, breachVolume, breachPrice, priceSource,
+				executionService);
+		timer = new Timer();
+		timer.scheduleAtFixedRate(priceListenerImpl, 0, Long.MAX_VALUE);
+	}
 }
